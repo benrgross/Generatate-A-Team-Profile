ffdd23b50ebe7c928884663c7ff495ee1fba264a
Ben Gross <benrgross@gmail.com>
2021-02-06 23:08:17 -0500

    folder for objects." "

---
+++ run.js
@@ -0,0 +1,35 @@
+// =========== Dependencies ============
+// ------- jest --------
+// ------ inquirer -------
+// ------ render html ------
+// -----------class constructor employee ---------
+// -- name
+// -- id
+// -- email - opens to default main with address in new email
+// -- getName()
+// -- getId()
+// -- getEmail()
+// -- getRole() -- returns Employee
+//------------ extend class -------------
+//----- MANAGER ----
+// -- officeNumber
+// -- getRole() - overrides to Manager
+//----- ENGINEER ----
+// -- github -- opens in new window
+// -- getGutHub()
+// -- getRole() -- overrides to Engineer
+//----- INTERN -----
+// -- school
+// -- getSchool()
+// -- getRole()  -- overrides to Intern
+
+//======= Functions ========
+// ------ get the answers from inquier after --> push to array
+// ------ wait till the user is done then render array to html
+
+// ====== User Interaction =====
+// -------------------- Inquirer questions ---------------------
+// -- first question: specify team member  -- choice
+// ------- need to figure out how to rerun this till user chooses generate html
+// ------------------ call back functions within inquirer?
+// -- switch case for each person ?
