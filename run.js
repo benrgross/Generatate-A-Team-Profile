fd2c4b69b762299269f2035511ab09def7618997
Ben Gross <benrgross@gmail.com>
2021-02-06 23:48:49 -0500

    make dependencies.

--- run.js
+++ run.js
@@ -1,33 +1,18 @@
 // =========== Dependencies ============
-// ------- jest --------
-// ------ inquirer -------
-// ------ render html ------
-// -----------class constructor employee ---------
-// -- name
-// -- id
-// -- email - opens to default main with address in new email
-// -- getName()
-// -- getId()
-// -- getEmail()
-// -- getRole() -- returns Employee
-//------------ extend class -------------
-//----- MANAGER ----
-// -- officeNumber
-// -- getRole() - overrides to Manager
-//----- ENGINEER ----
-// -- github -- opens in new window
-// -- getGutHub()
-// -- getRole() -- overrides to Engineer
-//----- INTERN -----
-// -- school
-// -- getSchool()
-// -- getRole()  -- overrides to Intern
+const inquirer = require("inquirer");
+const jest = require("jest");
+
+const Employee = require("../Employee");
+const Manager = require("./objects/Manager");
+const Intern = require("../Intern");
+const Engineer = require("../Engineer");
 
 //======= Functions ========
 // ------ get the answers from inquier after --> push to array
 // ------ wait till the user is done then render array to html
 
 // ====== User Interaction =====
+
 // -------------------- Inquirer questions ---------------------
 // -- first question: specify team member  -- choice
 // ------- need to figure out how to rerun this till user chooses generate html
